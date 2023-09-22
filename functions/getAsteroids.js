a5385c936a613ec053acae265354a677f6702db4
Bankn8II©$A <102619282+barionleg@users.noreply.github.com>
2023-09-22 15:25:23 +0200

    Update getAsteroids.js

--- functions/getAsteroids.js
+++ functions/getAsteroids.js
@@ -5,7 +5,7 @@ exports.handler = async (event, context) => {
   const date = event.queryStringParameters.date;
   const API_URL = "https://api.nasa.gov/neo/rest/v1/feed";
   const API_KEY = process.env.NASA_API_KEY;
-  const apiURL = `${API_URL}?api_key=${DEMO_KEY}&start_date=${date}&end_date=${date}`;
+  const apiURL = `${API_URL}?api_key=${API_KEY}&start_date=${date}&end_date=${date}`;
   //
   const response = await fetch(apiURL);
   const data = await response.json();
